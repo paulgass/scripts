838bcc208b96337af80084d0bdeb2c10d75bdbc2
PAUL GASS <38982740+paulgass@users.noreply.github.com>
2020-04-21 01:11:50 -0400

    Update checkcommand.sh

--- checkcommand.sh
+++ checkcommand.sh
@@ -3,7 +3,7 @@
 echo test1test1test1
 
 workingtest="xxx3.2.3xxx"
-if [[ $workingtest =~ *[0-9].* ]];then
+if [[ $workingtest = *[0-9].* ]];then
   echo "result contains [0-9]. inside of it"
 else
   echo "result does not contain [0-9]. inside of it"
@@ -12,7 +12,7 @@ fi
 echo test2test2test2
 
 workingtest2="xxx3.2.3xxx"
-if [[ $workingtest2 =~ *[0-9].* ]];then
+if [[ $workingtest2 = *[0-9].* ]];then
   echo "result contains [0-9]. inside of it"
 else
   echo "result does not contain [0-9]. inside of it"
@@ -21,7 +21,7 @@ fi
 echo test3test3test3whichissameastest1test1test1
 
 workingtest="xxx3.2.3xxx"
-if [[ $workingtest =~ *[0-9].* ]];then
+if [[ $workingtest = *[0-9].* ]];then
   echo "result contains [0-9]. inside of it"
 else
   echo "result does not contain [0-9]. inside of it"
@@ -31,10 +31,17 @@ echo test4test4test4
 
 checkcommandtest=$(python --version)
 
-if [[ $checkcommandtest =~ *[0-9].* ]];then
+if [[ $checkcommandtest = *[0-9].* ]];then
   echo "result contains [0-9]. inside of it"
 else
   echo "result does not contain [0-9]. inside of it"
 fi
 
 echo test5test5test5
+
+workingtest5="xxxxxxxxx"
+if [[ $workingtest5 = *[0-9].* ]];then
+  echo "result contains [0-9]. inside of it"
+else
+  echo "result does not contain [0-9]. inside of it"
+fi
