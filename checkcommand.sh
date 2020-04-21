253104cd1057b0613e96bb5a78443bab35eb4cad
PAUL GASS <38982740+paulgass@users.noreply.github.com>
2020-04-21 01:06:53 -0400

    Create checkcommand.sh

---
+++ checkcommand.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo test1test1test1
+
+workingtest="xxx3.2.3xxx"
+if [[ $workingtest =~ *[0-9].* ]];then
+  echo "result contains [0-9]. inside of it"
+else
+  echo "result does not contain [0-9]. inside of it"
+fi
+
+echo test2test2test2
+
+workingtest2="xxx3.2.3xxx"
+if [[ $workingtest2 =~ *[0-9].* ]];then
+  echo "result contains [0-9]. inside of it"
+else
+  echo "result does not contain [0-9]. inside of it"
+fi
+
+echo test3test3test3whichissameastest1test1test1
+
+workingtest="xxx3.2.3xxx"
+if [[ $workingtest =~ *[0-9].* ]];then
+  echo "result contains [0-9]. inside of it"
+else
+  echo "result does not contain [0-9]. inside of it"
+fi
+
+echo test4test4test4
+
+checkcommandtest=$(python --version)
+
+if [[ $checkcommandtest =~ *[0-9].* ]];then
+  echo "result contains [0-9]. inside of it"
+else
+  echo "result does not contain [0-9]. inside of it"
+fi
+
+echo test5test5test5
