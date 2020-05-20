b3c01d12aee1088b9cb5b067687f9269a763dc54
Gass, Paul CTR <paul.gass@isso.net>
2020-05-20 17:18:32 -0400

    packagemanager27

--- default.sh
+++ default.sh
@@ -2,35 +2,39 @@
 
 #OS AGNOSTIC SCRIPT
 #maybe change to #!/bin/sh
-#this scripts should work for the following:
-#debian
-#ubuntu
-#centos
-#fedora
-#openSUSE
-#arch
-#macoszsh
-#macosbash
-
-if [[ $EUID -ne 0 ]]; then
-   echo "THIS SCRIPT MUST BE RUN AS 'root' USER"
-   exit 1
-fi
+#this scripts should work for the following: #debian #ubuntu #centos #fedora #openSUSE #arch#macoszsh #macosbash
 
 packagemanager=""
-lsbversionnumber=$(lsb_release --short --release)
+lsbreleaseversionnumber=$(lsb_release --short --release)
 
-if [[ $lsbversionnumber = *[0-9]* ]] ; then
+if [[ $lsbreleaseversionnumber = *[0-9]* ]]
+then
    packagemanager="default"
 else
-   echo "lsb_release command not found."
+   break
 fi
 
 packagemanagerversion () {
    $1 --version >pmv.txt 2>1
    v=$(cat pmv.txt)
-   if [[ $v = *[0-9]* ]] ; then
-      echo "$1 package manger found."
+   if [[ $v = *[0-9]* ]]
+   then
+      if [[ $1 = "yum" ]]
+      then
+         sudo yum -y update && sudo yum -y install redhat-lsb-core
+      elif [ $1 == 'dnf' ]
+      then
+         sudo dnf -y update && sudo dnf -y install redhat-lsb-core
+      elif [ $1 == 'apt-get' ]
+      then
+         sudo apt-get update -y && sudo apt-get install -y lsb-core
+      elif [ $1 == 'zypper' ]
+      then
+         sudo zypper update -y && sudo zypper install -y lsb-core
+      elif [ $1 == 'pacman' ]
+      then
+         pacman -Syu lsb-release
+      fi
    else
       echo "$1 package manger NOT found."
    fi
