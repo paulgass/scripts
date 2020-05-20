a6c1ee8da81b4c2f572448adbff3efa0cf2699bc
PAUL GASS <38982740+paulgass@users.noreply.github.com>
2020-05-20 12:11:09 -0400

    Update default.sh

--- default.sh
+++ default.sh
@@ -18,44 +18,80 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 NOWSECOND=$(date +"%Y%m%d%H%M%S")
-
 scriptfolder=script${NOWSECOND}folder
-
 mkdir $scriptfolder
-
 cd $scriptfolder/
-
 directory=$(pwd)
-
 cd $directory/
 
+print_something () {
+   echo Hello $1
+}
+
+print_something Mars
+print_something Jupiter
+
+#apt --version >>versionapt.txt 2>&1
+
+#apt-get --version >>versionaptget.txt 2>&1
+
+#rpm --version >>versionrpm.txt 2>&1
+
+#yum --version >>versionyum.txt 2>&1
+
+#dnf --version >>versiondnf.txt 2>&1
+
+#zypper --version >>versionzypper.txt 2>&1
+
+#pacman --version >>versionpacman.txt 2>&1
 packagemanager=""
 
-echo "Now scanning to find configured Package Manager for this system..."
-echo "apt..."
+echo "...lsb_release..."
+
+lsb_release --version >>versionlsbrelease.txt 2>&1
+
+versionlsbrelease=$(cat versionlsbrelease.txt)
+
+if [ $versionlsbrelease = *[0-9].* ] ; then
+   packagemanager="default"
+else
+   echo "lsb_release command not found."
+fi
+
+echo "...rpm..."
+
+apt --version >>versionrpm.txt 2>&1
+
+versionrpm=$(cat versionrpm.txt)
+
+if [ $packagemanager != "" ] && [ $versionrpm = *[0-9].* ] ; then
+   packagemanager="rpm"
+else
+   echo "RPM configuration not found."
+fi
+
+echo "...apt..."
 
 apt --version >>versionapt.txt 2>&1
 
 versionapt=$(cat versionapt.txt)
 
-if [[ $versionapt = *[0-9].* ]];then
-  echo "this system is configured with apt Package Manager"
-  packagemanager="apt"
+if [ $packagemanager != "" ] && [ $versionapt = *[0-9].* ] ; then
+   packagemanager="apt"
 else
-  echo "this system is NOT configured with apt Package Manager"
+   echo "APT configuration not found."
 fi
 
-echo "apt-get..."
+echo "...apt-get..."
 
-apt --version >>versionaptget.txt 2>&1
+apt-get --version >>versionaptget.txt 2>&1
 
 versionaptget=$(cat versionaptget.txt)
 
-if [[ $versionaptget = *[0-9].* ]];then
-  echo "this system is configured with apt-get Package Manager"
-  packagemanager="apt-get"
+if [ $packagemanager != "" ] && [ $versionaptget = *[0-9].* ] ; then
+   packagemanager="apt-get"
 else
-  echo "this system is NOT configured with apt-get Package Manager"
+   echo "APT-GET configuration not found."
 fi
 
 echo goodnight
