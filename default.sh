e1ce0f88ca672877a3029a1304dd05e1438b1e6b
Gass, Paul CTR <paul.gass@isso.net>
2020-05-21 11:00:11 -0400

    default script 1

--- default.sh
+++ default.sh
@@ -1,49 +1,69 @@
 #!/bin/bash
 
-#OS AGNOSTIC SCRIPT
-#maybe change to #!/bin/sh
-#this scripts should work for the following: #debian #ubuntu #centos #fedora #openSUSE #arch#macoszsh #macosbash
-
-packagemanagerversion () {
+checksystemforpackagemanager () {
    $1 --version >pmv.txt 2>1
    v=$(cat pmv.txt)
-   if [[ $v = *[0-9]* ]]
+   if [[ $v != *[0-9]* ]]
    then
-      if [[ $1 = "yum" ]]
-      then
-         sudo yum -y update && sudo yum -y install redhat-lsb-core
-      elif [ $1 == 'dnf' ]
-      then
-         sudo dnf -y update && sudo dnf -y install redhat-lsb-core
-      elif [ $1 == 'apt-get' ]
-      then
-         sudo apt-get update -y && sudo apt-get install -y lsb-core
-      elif [ $1 == 'zypper' ]
-      then
-         sudo zypper update -y && sudo zypper install -y lsb-core
-      elif [ $1 == 'pacman' ]
-      then
-         pacman -Syu lsb-release
-      fi
-   else
       echo "$1 package manger NOT found."
    fi
+   rm pmv.txt
 }
 
-packagemanager=""
+defaultpackagemanager="none"
 lsbreleaseversionnumber=$(lsb_release --short --release)
 
 if [[ $lsbreleaseversionnumber = *[0-9]* ]]
 then
-   packagemanager="default"
+   os=$(lsb_release --short --id)
+   if [[ $os = *centos* ]]
+   then
+      defaultpackagemanager="yum"
+   elif [[ $os = *debian* ]] || [[ $os = *ubuntu* ]]
+   then
+      defaultpackagemanager="apt-get"
+   elif [[ $os == *fedora* ]]
+   then
+      defaultpackagemanager="dnf"
+   elif [[ $os == *opensuse* ]]
+   then
+      defaultpackagemanager="zypper"
+   elif [[ $os == *arch* ]]
+   then
+      defaultpackagemanager="pacman"
+   else
+      echo "cannot determine package manager for $os"
+   fi
 fi
 
-if [[ $packagemanager != "default" ]] ; then
-   packagemanagerversion "yum"
-   packagemanagerversion "dnf"
-   packagemanagerversion "apt-get"
-   packagemanagerversion "zypper"
-   packagemanagerversion "pacman"
+if [[ $defaultpackagemanager == "none" ]]
+then
+   checksystemforpackagemanager "yum"
+   checksystemforpackagemanager "dnf"
+   checksystemforpackagemanager "apt-get"
+   checksystemforpackagemanager "zypper"
+   checksystemforpackagemanager "pacman"
+   checksystemforpackagemanager "brew"
+   checksystemforpackagemanager "snap"
+else
+   if [ defaultpackagemanager="yum" ]
+   then
+      sudo yum -y update && sudo yum -y install redhat-lsb-core
+   elif [ defaultpackagemanager="dnf" ]
+   then
+      sudo dnf -y update && sudo dnf -y install redhat-lsb-core
+   elif [ defaultpackagemanager="apt-get" ]
+   then
+      sudo apt-get update -y && sudo apt-get install -y lsb-core
+   elif [ defaultpackagemanager="zypper" ]
+   then
+      sudo zypper update -y && sudo zypper install -y lsb-core
+   elif [ defaultpackagemanager="pacman" ]
+   then
+      pacman -Syu lsb-release
+   else
+      echo "cannot install lsb_release"
+   fi
 fi
 
 echo goodnightlollol
