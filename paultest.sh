8623d65ae33d22b58db9d16995cb86ada04753ce
Gass, Paul CTR <paul.gass@isso.net>
2020-05-21 16:58:57 -0400

    lsbrelease11

--- paultest.sh
+++ paultest.sh
@@ -1,34 +1,21 @@
 #!/bin/bash
 
-checksystemforlsb () {
-   x=0
-   a=$(lsb_release --short --release)
-   if [[ $a = *[0-9]* ]]
-   then
-      x=1
-   fi
-   return $x
+echo "xxxxxxxxxxxx"
+
+function myfunc()
+{
+    local  __resultvar=$1
+    local  myresult='some value'
+    if [[ "$__resultvar" ]]; then
+        eval $__resultvar="'$myresult'"
+    else
+        echo "$myresult"
+    fi
 }
 
-systemlsb=$(checksystemforlsb)
-
-while [ systemlsb != 1 ]
-do
-   attemptlsbinstall "yum"
-   systemlsb=$(checksystemforlsb)
-   echo $systemlsb
-   attemptlsbinstall "dnf"
-   systemlsb=$(checksystemforlsb)
-   echo $systemlsb
-   attemptlsbinstall "apt-get"
-   systemlsb=$(checksystemforlsb)
-   echo $systemlsb
-   attemptlsbinstall "zypper"
-   systemlsb=$(checksystemforlsb)
-   echo $systemlsb
-   attemptlsbinstall "pacman"
-   systemlsb=$(checksystemforlsb)
-   echo $systemlsb
-done
-
-echo "$systemosname:$systemosversion"
+myfunc result
+echo "yyyyyyyyyyy"
+echo $result
+echo "zzzzzzzzzzz"
+result2=$(myfunc)
+echo $result2
