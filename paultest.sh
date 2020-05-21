f96f25cd191ecf2a2019d86c80300b06139aec2e
Gass, Paul CTR <paul.gass@isso.net>
2020-05-21 17:46:46 -0400

    lsbrelease20

--- paultest.sh
+++ paultest.sh
@@ -2,10 +2,16 @@
 
 function myfunc()
 {
-    local result=$1
-    local myresult='some value'
-    eval $result="'$myresult'"
+    local  __resultvar=$1
+    local  myresult='some value'
+    if [[ "$__resultvar" ]]; then
+        eval $__resultvar="'$myresult'"
+    else
+        echo "$myresult"
+    fi
 }
 
 myfunc result
 echo $result
+result2=$(myfunc)
+echo $result2
