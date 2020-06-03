efa93277a6f8703c040e3f7425786945784a5fab
PAUL GASS <38982740+paulgass@users.noreply.github.com>
2020-06-03 01:03:52 -0400

    Update WindowsPortProcessKill.bat

--- WindowsPortProcessKill.bat
+++ WindowsPortProcessKill.bat
@@ -1,23 +1,12 @@
 @ECHO OFF
-ECHO Step 1) Enter Port Number to Display ; Step 2) Enter Process Number to Kill
-ECHO.
 SET /P port=Display Port Number:
 ECHO %port%| findstr /r "^[1-9][0-9]*$">nul
 IF %errorlevel% equ 0 (
-	ECHO.
-	ECHO "Executing the Following Command: NETSTAT -ano | FINDSTR :%port%"
-	ECHO.
 	NETSTAT -ano | FINDSTR :%port%
-	ECHO.
 	setlocal EnableDelayedExpansion
 	SET /P process=Kill Port %port% Process Number:
 	ECHO !process!| findstr /r "^[1-9][0-9]*$">nul
 	IF %errorlevel% equ 0 (
-		ECHO.
-		ECHO "Executing the Following Command: TASKKILL /PID !process! /F"
-		ECHO.
-		PAUSE
 		TASKKILL /PID !process! /F
-		PAUSE
 	)
 )
